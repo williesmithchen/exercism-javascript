e093856b55dfb65bdac4cd73fc8d14308933283f
Willie.Chen <williesmithchen@gmail.com>
2019-01-17 15:49:20 +0800

    Leap v2

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,1 +1,7 @@
-export const isLeap = (year) => ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+class Leap {
+  static isLeap (year) {
+    return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+  }
+}
+
+export const isLeap = (year) => Leap.isLeap(year);
